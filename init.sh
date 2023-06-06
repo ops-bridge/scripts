7ec7f0321d2e6ad8ed2ce19d3706e2c02ce48221
opshubb <dogus.peynirci@arabam.com>
2023-06-06 21:33:44 +0300

    ci

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install vim wget git net-tools telnet curl nload -y
+sudo apt install vim wget git net-tools telnet curl nload socat conntrack -y
 sudo timedatectl set-timezone Europe/Istanbul
 sudo hostnamectl set-hostname tenant-opsbridge01
 
@@ -26,3 +26,31 @@ sudo sed -i 's/FallbackNTP=ntp02.arabam.com/FallbackNTP=1.tr.pool.ntp.org/g' /et
 
 sudo systemctl restart systemd-timesyncd
 sudo apt autoremove -y
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install containerd.io
+cat > /etc/containerd/config.toml <<EOF
+[plugins."io.containerd.grpc.v1.cri"]
+  systemd_cgroup = true
+EOF
+cat > /etc/crictl.yaml <<EOF
+runtime-endpoint: unix:///var/run/containerd/containerd.sock
+image-endpoint: unix:///run/containerd/containerd.sock
+timeout: 2
+debug: true
+pull-image-on-create: false
+disable-pull-on-run: false
+EOF
+sudo systemctl restart containerd
+curl -sfL https://get-kk.kubesphere.io | VERSION=v3.0.7 sh -
+chmod +x kk
+./kk version --show-supported-k8s
+./kk create cluster --with-kubernetes v1.24.9 --with-kubesphere v3.3.2 --container-manager containerd
