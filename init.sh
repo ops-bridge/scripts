7a8d9b32121b5658981f1079cf4cee772e255d3a
opshubb <dogus.peynirci@arabam.com>
2023-06-06 14:33:55 +0300

    ci

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install vim wget git net-tools telnet curl nload -y
+sudo apt install vim wget git net-tools telnet curl nload yq jq -y
 sudo timedatectl set-timezone Europe/Istanbul
 sudo hostnamectl set-hostname tenant-opsbridge01
 
@@ -16,7 +16,7 @@ sudo resize2fs /dev/ubuntu-vg/ubuntu-lv
 
 # Install yq Tool
 
-sudo wget https://github.com/mikefarah/yq/releases/download/v4.2.0/yq_linux_amd64.tar.gz -O - | tar xz && mv yq_linux_amd64 /usr/bin/yq
+# sudo wget https://github.com/mikefarah/yq/releases/download/v4.2.0/yq_linux_amd64.tar.gz -O - | tar xz && mv yq_linux_amd64 /usr/bin/yq
 
 # Disable Automatic Upgrades
 
