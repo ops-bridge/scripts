ead96f2154ae1b0762d27506060987904baa071c
opshubb <dogus.peynirci@arabam.com>
2023-06-06 14:35:30 +0300

    ci

--- init.sh
+++ init.sh
@@ -14,10 +14,6 @@ sudo hostnamectl set-hostname tenant-opsbridge01
 sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
 sudo resize2fs /dev/ubuntu-vg/ubuntu-lv
 
-# Install yq Tool
-
-# sudo wget https://github.com/mikefarah/yq/releases/download/v4.2.0/yq_linux_amd64.tar.gz -O - | tar xz && mv yq_linux_amd64 /usr/bin/yq
-
 # Disable Automatic Upgrades
 
 sudo sed -i 's/APT::Periodic::Unattended-Upgrade "0";/APT::Periodic::Unattended-Upgrade "1";/g' /etc/apt/apt.conf.d/20auto-upgrades
