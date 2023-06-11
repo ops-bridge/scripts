4f727665da5a884f75dce82581ea5e3886d942a4
opshubb <dogus.peynirci@arabam.com>
2023-06-11 22:30:21 +0300

    ci

--- init.sh
+++ init.sh
@@ -1,62 +1,65 @@
 #!/bin/bash
 
-# Update OS
-
-sudo apt update
-sudo apt upgrade -y
-sudo apt install vim wget git net-tools telnet curl nload socat conntrack -y
-sudo timedatectl set-timezone Europe/Istanbul
-sudo hostnamectl set-hostname tenant-opsbridge01
-
-# Extend Default Ubuntu OS Disk Size
-
-sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
-sudo resize2fs /dev/ubuntu-vg/ubuntu-lv
-
-# Disable Automatic Upgrades
-
-sudo sed -i 's/APT::Periodic::Unattended-Upgrade "1";/APT::Periodic::Unattended-Upgrade "0";/g' /etc/apt/apt.conf.d/20auto-upgrades
-
-# Change NTP Servers
-
-sudo sed -i 's/NTP=ntp01.arabam.com/NTP=0.tr.pool.ntp.org/g' /etc/systemd/timesyncd.conf
-sudo sed -i 's/FallbackNTP=ntp02.arabam.com/FallbackNTP=1.tr.pool.ntp.org/g' /etc/systemd/timesyncd.conf
-
-# Restart Timesync Service On Ubuntu Server
-
-sudo systemctl restart systemd-timesyncd
-sudo apt autoremove -y
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install containerd.io
-cat > /etc/containerd/config.toml <<EOF
-[plugins."io.containerd.grpc.v1.cri"]
-  systemd_cgroup = true
+PS3='Please enter your choice: '
+options=("Prepare Operating System" "Install Containerd Runtime" "Install Kubernetes" "Uninstall Kubernetes" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Prepare Operating System")
+            sudo apt update
+            sudo apt upgrade -y
+            sudo apt install vim wget git net-tools telnet curl nload socat conntrack -y
+            sudo timedatectl set-timezone Europe/Istanbul
+            sudo hostnamectl set-hostname opsbridge01
+            sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
+            sudo resize2fs /dev/ubuntu-vg/ubuntu-lv
+            sudo sed -i 's/APT::Periodic::Unattended-Upgrade "1";/APT::Periodic::Unattended-Upgrade "0";/g' /etc/apt/apt.conf.d/20auto-upgrades
+            sudo sed -i 's/NTP=ntp01.arabam.com/NTP=0.tr.pool.ntp.org/g' /etc/systemd/timesyncd.conf
+            sudo sed -i 's/FallbackNTP=ntp02.arabam.com/FallbackNTP=1.tr.pool.ntp.org/g' /etc/systemd/timesyncd.conf
+            sudo systemctl restart systemd-timesyncd
+            sudo apt autoremove -y
+            ;;
+        "Install Containerd Runtime")
+            sudo apt-get install ca-certificates curl gnupg
+            sudo install -m 0755 -d /etc/apt/keyrings
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+            sudo chmod a+r /etc/apt/keyrings/docker.gpg
+            echo \
+              "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+              "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+              sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            sudo apt-get update
+            sudo apt-get install containerd.io
+            cat > /etc/containerd/config.toml <<EOF
+            [plugins."io.containerd.grpc.v1.cri"]
+              systemd_cgroup = true
 EOF
-cat > /etc/crictl.yaml <<EOF
-runtime-endpoint: unix:///var/run/containerd/containerd.sock
-image-endpoint: unix:///run/containerd/containerd.sock
-timeout: 2
-debug: true
-pull-image-on-create: false
-disable-pull-on-run: false
+            cat > /etc/crictl.yaml <<EOF
+            runtime-endpoint: unix:///var/run/containerd/containerd.sock
+            image-endpoint: unix:///run/containerd/containerd.sock
+            timeout: 2
+            debug: false
+            pull-image-on-create: false
+            disable-pull-on-run: false
 EOF
-sudo systemctl restart containerd
-curl -sfL https://get-kk.kubesphere.io | VERSION=v3.0.7 sh -
-chmod +x kk
-./kk version --show-supported-k8s
-echo yes | ./kk create cluster --with-kubernetes v1.24.9 --with-kubesphere v3.3.2 --container-manager containerd
+            sudo systemctl restart containerd
+            ;;
+        "Install Kubernetes")
+            curl -sfL https://get-kk.kubesphere.io | VERSION=v3.0.7 sh -
+            chmod +x kk
+            ./kk version --show-supported-k8s
+            echo yes | ./kk create cluster --with-kubernetes v1.24.9 --with-kubesphere v3.3.2 --container-manager containerd
+            sudo apt-get install bash-completion -y
+            echo 'source <(kubectl completion bash)' >>~/.bashrc
+            kubectl completion bash >/etc/bash_completion.d/kubectl
+            ;;
+        "Uninstall Kubernetes")
+            echo yes | ./kk delete cluster
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
 
-# Install bash-completion
-
-sudo apt-get install bash-completion -y
-echo 'source <(kubectl completion bash)' >>~/.bashrc
-kubectl completion bash >/etc/bash_completion.d/kubectl
--- uninstall.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo yes | ./kk delete cluster
-rm -rf 
